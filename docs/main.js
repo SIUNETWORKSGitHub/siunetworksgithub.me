3e30f3b36d5f99aa8e5c07f0d2e716be96de440a
siunetworksgithub <77218998+SIUNETWORKSGitHub@users.noreply.github.com>
2023-12-24 18:45:27 -0500

    Add files via upload

---
+++ docs/main.js
@@ -0,0 +1,18 @@
+window.addEventListener( 'DOMContentLoaded', (Event) =>{
+    getVisitCount();
+})    
+const functionApi = '';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
